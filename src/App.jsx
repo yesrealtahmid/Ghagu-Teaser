b76bb2a071ec9f0e11e8afa3be1de520c5976a0a
yesrealtahmid <redfoxtahmid@gmail.com>
2024-11-20 23:19:02 +0600

    added perfect size in the logo

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ const App = () => {
   return (
     <main className='container mx-auto'>
       <nav className='flex items-center justify-between container mx-auto px-5'>
-        <a href="#"><img className='w-48' src={logo} alt="" /></a>
+        <a href="#"><img className='w-32 lg:w-48' src={logo} alt="" /></a>
         <ul className='flex items-center gap-9'>
           <li className='text-white'><a href="https://www.facebook.com/ghaguEmerald" target='_blank'>Facebook</a></li>
           <li className='text-white'><a href="https://www.youtube.com/@GhaguEmerald" target='_blank'>Youtube</a></li>
         </ul>
       </nav>
-      <div className='youtube-Player mt-20'>
+      <div className='youtube-Player my-20'>
         <VideoIframe videoId={'mO-qxAJwD34'}></VideoIframe>
       </div>
     </main>
